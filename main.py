2e7b63c3d019ceded895ccb3a7f3249cb4726ba2
Dan Sanderson <contact@dansanderson.com>
2014-03-21 19:10:18 -0700

    Initial commit of a Python Bottle App Engine sample.

---
+++ main.py
@@ -0,0 +1,23 @@
+"""`main` is the top level module for your Bottle application."""
+
+# import the Bottle framework
+from bottle import Bottle
+
+# Create the Bottle WSGI application.
+bottle = Bottle()
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+
+# Define an handler for the root URL of our application.
+@bottle.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World'
+
+
+# Define an handler for 404 errors.
+@bottle.error(404)
+def error_404(error):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.'
